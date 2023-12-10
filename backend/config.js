aee2e3054792c7b20553820dd69b545ea0154737
Mike Shippy <mikeship04@gmail.com>
2023-12-10 12:37:41 -0700

    fix

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const port = 4000;
 
-export const mongoURL = `mongodb+srv://root:test@cluster0.gp2qhhz.mongodb.net/books-collection?retryWrites=true&w=majority`
+export const mongoURL = mongourl
